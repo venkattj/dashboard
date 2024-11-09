d4a1001d1ceef9d3d53cb2435fbd4685be68090a
venkattj <37686279+venkattj@users.noreply.github.com>
2024-11-10 02:03:11 +0530

    server

--- dashboard-app/src/api.js
+++ dashboard-app/src/api.js
@@ -2,9 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
-//  baseURL: '/',
-
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 export default api;
