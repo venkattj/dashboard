a9dd66b885a983e62c134b06be949d595c20f82c
venkattj <37686279+venkattj@users.noreply.github.com>
2026-03-29 01:58:29 +0530

    delete every thing

--- dashboard-app/src/api.js
+++
@@ -1,8 +0,0 @@
-// api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-});
-
-export default api;
--- dashboard-rest/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'teja@4795'
-    MYSQL_DB = 'teja'
